f8aeae21b87bd043476f709c107d98a431dce8b4
aswinss18 <aswinartography18@gmail.com>
2024-07-30 22:25:20 +0530

    Focusing and reseting on enter key pressing functionalities are created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useInsertionEffect, useRef, useState } from "react";
 import "./App.css";
 import StarRating from "./StarRating";
 const tempMovieData = [
@@ -204,6 +204,20 @@ function NumResults({ movies = [] }) {
   );
 }
 function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(function () {
+    function callback(e) {
+      if (document.activeElement === inputEl.current) return;
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery("");
+      }
+    }
+    document.addEventListener("keydown", callback);
+    return () => document.removeEventListener("keydown", callback);
+  }, []);
+
   return (
     <input
       className="search"
@@ -211,6 +225,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
